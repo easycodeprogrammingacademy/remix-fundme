6517b1e1bcf0aee62d98cbf4e2a99f153ba9e54a
Easycode Programming Academy <161119691+easycodeprogrammingacademy@users.noreply.github.com>
2024-02-28 00:34:58 +0800

    Update PriceConverter.sol

--- PriceConverter.sol
+++ PriceConverter.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.24;
 import {AggregatorV3Interface} from "@chainlink/contracts@0.8.0/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
-    function getEthToUsdPrice() public view returns (uint256) {
+    function getEthToUsdPrice() internal view returns (uint256) {
         // Address, Abi
         AggregatorV3Interface dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         uint8 decimals = dataFeed.decimals();
@@ -18,7 +18,7 @@ library PriceConverter {
         return uint256(answer) * (10 ** (18 - decimals));
     }
 
-    function convertEthToUsd(uint256 ethAmount) public view returns (uint256) {
+    function convertEthToUsd(uint256 ethAmount) internal view returns (uint256) {
         uint256 currentEthPrice = getEthToUsdPrice();
         return (ethAmount * currentEthPrice) / 1e18;
     }
