9031c7b69951a0d872feb65020ba5d8496732eaa
Easycode Programming Academy <161119691+easycodeprogrammingacademy@users.noreply.github.com>
2024-02-28 00:08:12 +0800

    Create PriceConverter.sol

---
+++ PriceConverter.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {AggregatorV3Interface} from "@chainlink/contracts@0.8.0/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    function getEthToUsdPrice() public view returns (uint256) {
+        // Address, Abi
+        AggregatorV3Interface dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        uint8 decimals = dataFeed.decimals();
+        (/* uint80 roundID */,
+            int256  answer,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = dataFeed.latestRoundData();
+        return uint256(answer) * (10 ** (18 - decimals));
+    }
+
+    function convertEthToUsd(uint256 ethAmount) public view returns (uint256) {
+        uint256 currentEthPrice = getEthToUsdPrice();
+        return (ethAmount * currentEthPrice) / 1e18;
+    }
+}
